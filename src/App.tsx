6815a9dac6ce0606ed21d0ba2696bcea311cc29b
Annie Huang <annie.hn@gmail.com>
2022-07-12 21:53:07 +1000

    Another example of Autocomplete, which got more props configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import MuiAutocomplete from './components/MuiAutocomplete';
 import MuiAutocomplete2 from './components/MuiAutocomplete2';
+import MuiAutocomplete3 from './components/MuiAutocomplete3';
 
 function App() {
   return (
     <div className='App'>
       {/*<MuiAutocomplete />*/}
-      <MuiAutocomplete2 />
+      {/*<MuiAutocomplete2 />*/}
+      <MuiAutocomplete3 />
     </div>
   );
 }
---
+++ src/components/MuiAutocomplete3.tsx
@@ -0,0 +1,49 @@
+import { Autocomplete, Box, TextField } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import { useEffect, useState } from 'react';
+
+type Player = {
+  id: number;
+  first_name: string;
+  last_name: string;
+};
+
+const MuiAutocomplete3 = () => {
+  const [jsonResults, setJsonResults] = useState<Player[]>([]);
+
+  useEffect(() => {
+    fetch('https://www.balldontlie.io/api/v1/players')
+      .then((response) => response.json())
+      .then((json) => setJsonResults(json.data));
+  }, []);
+
+  console.log('jsonResults=', jsonResults);
+
+  return (
+    <Stack sx={{ width: 300, margin: 'auto' }}>
+      <Autocomplete
+        // complusory fields are options | renderInput
+        id='nba_player_demo'
+        getOptionLabel={(jsonResult: Player) =>
+          `${jsonResult.first_name} ${jsonResult.last_name}`
+        }
+        options={jsonResults}
+        sx={{ width: 300 }}
+        isOptionEqualToValue={(option, value) =>
+          option.first_name === value.first_name
+        }
+        noOptionsText={'NO AVAILABLE PLAYERS DAYUM'}
+        renderOption={(props, jsonResult) => (
+          <Box component='li' {...props} key={jsonResult.id}>
+            {jsonResult.first_name} {jsonResult.last_name}
+          </Box>
+        )}
+        renderInput={(params) => (
+          <TextField {...params} label='Search for a Player' />
+        )}
+      />
+    </Stack>
+  );
+};
+
+export default MuiAutocomplete3;
